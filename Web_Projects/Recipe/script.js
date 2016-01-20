af4a8424a462121c8c8ba9acb45d649ab538b96d
gueletk <gueletina.ksenia@gmail.com>
2016-01-19 23:11:40 -0500

    Added jQuery requirements to recipe page

--- Web_Projects/Recipe/script.js
+++ Web_Projects/Recipe/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-  console.log("Script included!");
-  console.log("test");
-  
+var images = true
+
+	$('input[type="checkbox"]').click(function() {
+	    if (this.checked) {
+	        //alert('Checked');
+	        $(this).siblings().addClass('crossed-out');
+	    } else {
+	        //alert('Unchecked');
+	        $(this).siblings().removeClass('crossed-out');
+	    }
+	});
+
+	$(".hide-image").click(function(){
+		if(images == true){
+			$("img").hide("slow");
+			images = false;
+			$(this).text("Show Images");
+		}
+		else{
+			$("img").show("slow");
+			images = true;
+			$(this).text("Hide Images");
+		}
+		
+	});
 });
